b1be39291deed1bec8b6833815e4da0ead8e2aac
LeonaYanX <149185088+LeonaYanX@users.noreply.github.com>
2023-12-30 19:11:57 +0400

    Odd and Even

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        System.Console.WriteLine("Enter the start point");
+        int startPoint = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine("Enter the end point");
+        int endPoint = Convert.ToInt32(Console.ReadLine());
+        int countEven = 0;
+        int countOdd = 0;
+        if (startPoint%2==0)
+        {
+            for(int i = startPoint; i<=endPoint ; i+=2)
+        {
+             countEven = countEven +1;
+        }
+        countOdd = (endPoint-startPoint) - countEven + 1;
+        Console.WriteLine($"Odd are = {countOdd}");
+         Console.WriteLine($"Even are = {countEven}");
+        }
+        if(startPoint%2!=0)
+        {
+            for(int j = startPoint; j<=endPoint ; j+=2)
+            {
+                countOdd = countOdd +1;
+            }
+         countEven = (endPoint-startPoint) - countOdd + 1;
+         Console.WriteLine($"Odd are = {countOdd}");
+         Console.WriteLine($"Even are = {countEven}");
+        }
+       
+        
+    }
+}
